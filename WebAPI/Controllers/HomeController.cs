704c87bc92225a54da2b4187dc99bab285ee9b1c
Rinku Rawat <rinku.rawat843@gmail.com>
2018-12-05 00:22:05 +0530

    Startup API

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebAPI.Controllers
 {
@@ -10,6 +12,8 @@ namespace WebAPI.Controllers
     {
         public ActionResult Index()
         {
+
+            
             ViewBag.Title = "Home Page";
 
             return View();
